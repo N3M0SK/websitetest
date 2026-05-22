0d4bfebd04b2816ca5e7c2b6107a8ec29192befe
N3M0SK <r0land.025025@gmail.com>
2026-05-22 21:21:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function sendMail(){
-	let parms = {
-		name : document.getElementById("name").value,
-		email : document.getElementById("email").value,
-		subject : document.getElementById("subject").value,
-		message : document.getElementById("message").value,
+	const parms = {
+		name : document.getElementById("#name").value,
+		email : document.getElementById("#email").value,
+		message : document.getElementById("#message").value,
 	}
 	
-	emailjs.send("service_4kukkea","template_18mjhu7",parms).(alert("Email Sent!!"))
+	emailjs.send("service_4kukkea","template_18mjhu7", parms)
+		.then(() => alert("email sent !!").catch(() => alert("email not sent!!")));	
 }
